88cc431474974e73cffbcd685882ece7df2f6633
Thomas Marquart <thomas@marquart.se>
2016-05-31 11:09:29 +0200

    play around with CPL median filter

--- cffitest.py
+++ cffitest.py
@@ -1,17 +1,21 @@
 #!/usr/bin/env python3
 
 import numpy as np
+import pylab as p
 from cffi import FFI
 
 ffi = FFI()
-ffi.cdef("void copy(float *in, float *out, int len);")
+ffi.cdef("void copy(float *in, float *out, int len); void medfilt(float *in, float *out, int len, int hw);")
 C = ffi.dlopen("cfuncs.so")
 
 def cast_double(a):
     return ffi.cast("float *", a.ctypes.data)
 
-a = 42*np.ones(16, dtype=np.float32)
+a = np.random.rand(45).astype(np.float32) + np.arange(45)/10
 b = np.zeros_like(a)
 
-C.copy(cast_double(a), cast_double(b), len(a))
+C.medfilt(cast_double(a), cast_double(b), len(a), 5)
 print(b)
+p.plot(a,'ko')
+p.plot(b,'-r')
+p.show()
--- cfuncs.c
+++ cfuncs.c
@@ -1,9 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <cpl.h>
 
-void copy(float *in, float *out, int len) {
+void copy(double *in, double *out, int len) {
     int i=0;
     for (i=0; i<len; i++) {
         out[i] = in[i];
     }
+}
+
+void double2vec(double * in, cpl_vector * out, int len) {
+    int i=0;
+    for (i=0; i<len; i++) {
+        cpl_vector_set(out,i,in[i]);
+    }
+}
+
+void vec2double(cpl_vector * in, double * out, int len) {
+    int i=0;
+    for (i=0; i<len; i++) {
+        out[i] = cpl_vector_get(in,i);
+    }
+}
+void medfilt(double *in, double *out, int len, int hw) {
+    cpl_vector *vec;
+    cpl_init(CPL_INIT_DEFAULT);
+    //vec1 = cpl_vector_new(len);
+    //double2vec(in,vec1,len);
+    vec = cpl_vector_wrap(len,in);
+    vec = cpl_vector_filter_median_create(vec, hw);
+    out = cpl_vector_unwrap(vec);
+    //vec2double(vec,out,len);
+    //cpl_vector_delete(vec1);
+    //cpl_vector_delete(vec2);
+
 }
