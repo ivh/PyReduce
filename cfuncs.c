4d67fe50f0dfbc5233eac1e4f1c3bfcb2a3a0d0b
Thomas Marquart <thomas@marquart.se>
2016-06-01 10:21:49 +0200

    copy the vector correctly into the output this time.

--- cfuncs.c
+++ cfuncs.c
@@ -2,36 +2,15 @@
 #include <stdlib.h>
 #include <cpl.h>
 
-void copy(double *in, double *out, int len) {
-    int i=0;
-    for (i=0; i<len; i++) {
-        out[i] = in[i];
-    }
-}
+void * memcpy ( void * destination, const void * source, size_t num );
 
-void double2vec(double * in, cpl_vector * out, int len) {
-    int i=0;
-    for (i=0; i<len; i++) {
-        cpl_vector_set(out,i,in[i]);
-    }
-}
-
-void vec2double(cpl_vector * in, double * out, int len) {
-    int i=0;
-    for (i=0; i<len; i++) {
-        out[i] = cpl_vector_get(in,i);
-    }
-}
 void medfilt(double *in, double *out, int len, int hw) {
+    double *dbl;
     cpl_vector *vec;
     cpl_init(CPL_INIT_DEFAULT);
-    //vec1 = cpl_vector_new(len);
-    //double2vec(in,vec1,len);
     vec = cpl_vector_wrap(len,in);
-    vec = cpl_vector_filter_median_create(vec, hw);
-    out = cpl_vector_unwrap(vec);
-    //vec2double(vec,out,len);
-    //cpl_vector_delete(vec1);
-    //cpl_vector_delete(vec2);
-
+    dbl = (double*)cpl_vector_unwrap( cpl_vector_filter_median_create(vec, hw) );
+    memcpy(out,dbl,len*sizeof(double));
+    free(dbl);
+    //cpl_vector_delete(vec); //No, this nulls the input-array!
 }
