651898f78465ed635f1b9eb2556f8d990f4d8aee
Thomas Marquart <thomas.marquart@fysast.uu.se>
2016-12-21 16:15:20 +0100

    betterPlot

--- Slit_Func/plot_model.py
+++ Slit_Func/plot_model.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 
-import sys
-print(sys.path)
-
 import numpy as np
 import matplotlib
-matplotlib.use('Qt4Agg')
-import matplotlib.pyplot as mpl
+matplotlib.use('QT4Agg')
+
+import matplotlib.pyplot as plt
 
 f = open('dump.bin')
 osample,ncols,nrows,ny = 10, 768, 15, 161
@@ -19,11 +17,14 @@ sP_old = np.fromfile(f, count=ncols, dtype=dt)
 im = np.fromfile(f, count=ncols*nrows, dtype=dt)
 model = np.fromfile(f, count=ncols*nrows, dtype=dt)
 
+foo = np.fromfile(f, count=1, dtype=dt)
+print(sL)
+
 f.close()
 
-F = mpl.Figure()
-ax1=F.add_subplot(1,3,1)
+fig, (ax1, ax2, ax3) = plt.subplots(ncols=3,nrows=1,squeeze=True,fig_kw=dict(figsize=(5,10)))
 
 ax1.plot(sL)
 
-mpl.show()
+
+plt.show()
