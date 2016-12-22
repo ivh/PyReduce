ed6145f9b0de2203357a760ca6a771db624cfc56
Thomas Marquart <thomas.marquart@fysast.uu.se>
2016-12-22 11:09:12 +0100

    get result plot done

--- Slit_Func/plot_model.py
+++ Slit_Func/plot_model.py
@@ -5,6 +5,10 @@ import matplotlib
 matplotlib.use('QT4Agg')
 
 import matplotlib.pyplot as plt
+plt.ioff()
+
+def midstep(ax, y):
+    return ax.step(np.arange(len(y)), y, where='mid')
 
 f = open('dump.bin')
 osample,ncols,nrows,ny = 10, 768, 15, 161
@@ -16,15 +20,22 @@ sP = np.fromfile(f, count=ncols, dtype=dt)
 sP_old = np.fromfile(f, count=ncols, dtype=dt)
 im = np.fromfile(f, count=ncols*nrows, dtype=dt)
 model = np.fromfile(f, count=ncols*nrows, dtype=dt)
+model.shape = nrows, ncols
 
 foo = np.fromfile(f, count=1, dtype=dt)
-print(sL)
 
 f.close()
 
-fig, (ax1, ax2, ax3) = plt.subplots(ncols=3,nrows=1,squeeze=True,fig_kw=dict(figsize=(5,10)))
+fig, (ax1, ax2, ax3) = plt.subplots(ncols=1,nrows=3,squeeze=True,figsize=(13,4))
 
-ax1.plot(sL)
+midstep(ax1,sL)
+ax1.set_title('sL')
+
+midstep(ax2,sP)
+ax2.set_title('sP')
+
+ax3.imshow(model)
+ax3.set_title('model')
 
 
 plt.show()
