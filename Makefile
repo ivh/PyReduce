1ecd456830376913c1f95163f49f0fa7a19d97ef
Thomas Marquart <thomas@marquart.se>
2018-06-18 19:59:02 +0200

    get basic wrapper for cluster.c in place, it builds. untested

--- Makefile
+++ Makefile
@@ -1,11 +1,11 @@
 all: clib/_cluster.so clib/_extract.so
 
-clib/_person.so: clib/build_cluster.py
-    python $<
+clib/_cluster.so: clib/build_cluster.py
+	python $<
 
-clib/_fnmatch.so: clib/build_extract.py
-    python $<
+clib/_extract.so: clib/build_extract.py
+	python $<
 
 clean:
-rm clib/_*.c clib/_*.o clib/_*.so
+	rm clib/_*.c clib/_*.o clib/_*.so
 
--- cwrappers.py
+++ cwrappers.py
@@ -1,21 +1,3 @@
-#!/usr/bin/env python3
 
-import numpy as np
-import pylab as p
-from cffi import FFI
+from .clib import cluster
 
-ffi = FFI()
-ffi.cdef("void copy(float *in, float *out, int len); void medfilt(float *in, float *out, int len, int hw);")
-C = ffi.dlopen("cfuncs.so")
-
-def cast_double(a):
-    return ffi.cast("float *", a.ctypes.data)
-
-a = np.random.rand(45).astype(np.float32) + np.arange(45)/10
-b = np.zeros_like(a)
-
-C.medfilt(cast_double(a), cast_double(b), len(a), 5)
-print(b)
-p.plot(a,'ko')
-p.plot(b,'-r')
-p.show()
