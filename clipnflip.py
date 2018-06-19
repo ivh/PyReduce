e026a63e21adcd84db52c5599d0c0df10c44b3a9
Ansgar Wehrhahn <ansgar.wehrhahn@physics.uu.se>
2018-06-19 13:58:08 +0200

    Clean up Combine_Frames & clipnflip

--- clipnflip.py
+++ clipnflip.py
@@ -2,7 +2,7 @@ import numpy as np
 import astropy.io.fits as fits
 
 
-def clipnflip(image, header, xr=None, yr=None, orient=None):
+def clipnflip(image, header, **kwargs):
     """
     # Process an image and associated FITS header already in memory as follows:
     # 1) Trim image to desired subregion: newimage = image(xlo:xhi,ylo:yhi)
@@ -28,80 +28,30 @@ def clipnflip(image, header, xr=None, yr=None, orient=None):
     """
 
     # Check that required arguments have proper structure.
-    err = None
-    bad = None
+
     if image.ndim != 2:
         raise Exception('image must be a two-dimensional array')
 
-    # ;Check the number of regions
-    # n_regions = sxpar(header, 'E_NREG', count=True)
-    # if count eq 0 then n_regions=1 else n_regions=n_regions>1
-
     # Make sure trim region is specificied by procedure or header keyword.
     # This part depends on how many amplifiers were used for the readout
-    try:
-        ver = header['e_hvers']
-    except KeyError:
-        ver = 0
-    if ver > 1.001:  # earlier versions did not support mutiple amplifiers
-        n_amp = header['e_ampl']
-    else:
-        n_amp = 1
+    # ver = header.get("e_hvers", 0)  # TODO default version number?
+    n_amp = header.get("e_ampl", 1)
 
     if n_amp > 1:  # more than one amplifier
-        try:
-            xlo = header['e_xlo*']
-        except KeyError:
-            bad = 'xlo'
-
-        try:
-            xhi = header['e_xhi*']
-        except KeyError:
-            bad = 'xhi'
-
-        try:
-            ylo = header['e_ylo*']
-        except KeyError:
-            bad = 'ylo'
-        try:
-            yhi = header['e_yhi*']
-        except KeyError:
-            bad = 'yhi'
-
-        # Check we have encountered inconsistent ranges
-        if bad is not None:
-            print(bad + ' not specified by argument or in header')
-            raise Exception('unable to trim image')
+        xlo = np.array(header['e_xlo*'].values())
+        xhi = np.array(header['e_xhi*'].values())
+        ylo = np.array(header['e_ylo*'].values())
+        yhi = np.array(header['e_yhi*'].values())
 
         # Make sure trim region is a subset of actual image.
         sz = image.shape
-        i1 = np.where((xlo < 0) | (xlo >= sz[0]))
-        i2 = np.where((ylo < 0) | (ylo >= sz[1]))
-        i3 = np.where((xhi < 0) | (xhi >= sz[0]))
-        i4 = np.where((yhi < 0) | (yhi >= sz[1]))
-        i5 = np.where(xlo >= xhi)
-        i6 = np.where(ylo >= yhi)
+        if (np.any(xlo < 0) | np.any(xlo >= sz[1]) | np.any(ylo < 0) |
+                np.any(ylo >= sz[0]) | np.any(xhi < 0) | np.any(xhi >= sz[1]) |
+                np.any(yhi < 0) | np.any(yhi >= sz[0])):
+            raise Exception('Error specifying trim region')
 
-        err = None
-        if len(i1) > 0:
-            err = 'error specifying x trim region:' + ' xlo'
-        if len(i2) > 0:
-            err = 'error specifying y trim region:' + ' ylo'
-        if len(i3) > 0:
-            err = 'error specifying x trim region:' + ' xhi'
-        if len(i4) > 0:
-            err = 'error specifying y trim region:' + ' yhi'
-        if len(i5) > 0:
-            err = 'error specifying x region boundaries:' + ' xlo>=xhi'
-        if len(i6) > 0:
-            err = 'error specifying y region boundaries:' + ' ylo>=yhi'
-        if err is not None:
-            print(err)
-            raise Exception('unable to trim image')
-
-        try:
-            linear = header['e_linear']
-        except KeyError:
+        linear = header.get('e_linear', False)
+        if not linear:
             pref = header['e_prefmo']
             # TODO
             raise NotImplementedError("only linear for now")
@@ -109,7 +59,7 @@ def clipnflip(image, header, xr=None, yr=None, orient=None):
 
             i = np.where(header['e_linear'] >= 0)
             if (len(i) > 0):
-                header = header[0:i - 1 + 1] +  header[i + 1:]
+                header = header[0:i - 1 + 1] + header[i + 1:]
             header['e_linear'] = ('t', 'image corrected of non-linearity')
             ii = np.where(header['e_gain*'] >= 0)
             if (len(ii) > 0):
@@ -122,8 +72,9 @@ def clipnflip(image, header, xr=None, yr=None, orient=None):
         # For two amplifiers we assume a single vertical or horizontal gap.
         # With four amplifiers we can have a cross.
 
-        if (n_amp == 2):
-            if (xlo[0] == xlo[1]):
+        if n_amp == 2:
+            # TODO this needs testing
+            if xlo[0] == xlo[1]:
                 xsize = xhi[0] - xlo[0] + 1
                 ysize = yhi[0] - ylo[0] + 1 + yhi[1] - ylo[1] + 1
                 timage = np.empty((xsize, ysize), dtype=image.dtype)
@@ -131,7 +82,7 @@ def clipnflip(image, header, xr=None, yr=None, orient=None):
                 timage[0:ysize - 1 + 1, 0:xsize - 1 + 1] = image[xlo[0]]
                 timage[ysize:ysize + yhi[1] - ylo[1] +
                        1, 0:xsize - 1 + 1] = image[xlo[1]]
-            elif (ylo[0] == ylo[1]):
+            elif ylo[0] == ylo[1]:
                 xsize = xhi[0] - xlo[0] + 1 + xhi[1] - xlo[1] + 1
                 ysize = yhi[0] - ylo[0] + 1
                 timage = np.empty((xsize, ysize), dtype=image.dtype)
@@ -140,68 +91,25 @@ def clipnflip(image, header, xr=None, yr=None, orient=None):
                 timage[0:ysize - 1 + 1, xsize:xsize +
                        xhi[1] - xlo[1] + 1] = image[xlo[1]]
             else:
-                print('the two ccd sections are aligned neither in x nor in y')
+                raise Exception(
+                    'The two ccd sections are aligned neither in x nor in y')
         elif (n_amp == 4):
-            print('4-amplifier section is not implemented yet')
+            raise NotImplementedError(
+                '4-amplifier section is not implemented yet')
     else:
-        if xr is not None:
-            xlo = xr[0]
-            xhi = xr[1]
-        else:
-            try:
-                xlo = header['e_xlo']
-            except KeyError:
-                bad = 'xlo'
-            try:
-                xhi = header['e_xhi']
-            except KeyError:
-                bad = 'xhi'
-        if yr is not None:
-            ylo = yr[0]
-            yhi = yr[1]
-        else:
-            try:
-                ylo = header['e_ylo']
-            except KeyError:
-                bad = 'ylo'
-            try:
-                yhi = header['e_yhi']
-            except KeyError:
-                bad = 'yhi'
-
-        # Check we have encountered inconsistent ranges
-        if bad is not None:
-            print(bad + ' not specified by argument or in header')
-            raise Exception('unable to trim image')
+        xlo, xhi = kwargs.get("xr", (header["e_xlo"], header["e_xhi"],))
+        ylo, yhi = kwargs.get("yr", (header["e_ylo"], header["e_yhi"],))
 
         # Make sure trim region is a subset of actual image.
         sz = image.shape
-        err = None
-        if xhi < xlo:
-            err = 'error specifying x trim region: xhi<xlo (%i < %i)' % (
-                xhi, xlo)
-        if yhi < ylo:
-            err = 'error specifying y trim region: yhi<ylo (%i < %i)' % (
-                yhi, ylo)
-        if xlo < 0 or xhi >= sz[1]:
-            err = 'x trim region [%i,%i] not contained in image (0<x<%i)' % (
-                xlo, xhi, sz[1] - 1)
-        if ylo < 0 or yhi >= sz[0]:
-            err = 'y trim region [%i,%i] not contained in image (0<y<%i)' % (
-                ylo, yhi, sz[0] - 1)
-        if err is not None:
-            print(err)
-            raise Exception('unable to trim image')
+        if xhi < xlo or yhi < ylo or xlo < 0 or xhi >= sz[1] or ylo < 0 or yhi >= sz[0]:
+            raise Exception('Could not trim region')
 
         # Trim image to leave only the subimage containing valid image data.
         timage = image[ylo:yhi + 1, xlo:xhi + 1]  # trimmed image
 
     # Flip image (if necessary) to achieve standard image orientation.
+    orient = kwargs.get("orient", header.get('e_orient'))
     if orient is not None:
-        try:
-            orient = header['e_orient']
-        except KeyError:
-            print('orient not specified by argument or in header')
-            raise Exception('unable to reorient image')
-        timage = np.rot90(timage, -1 * orient)
-    return timage.swapaxes(0, 1)
+        timage = np.rot90(timage, -orient)
+    return timage
--- combine_frames.py
+++ combine_frames.py
@@ -5,9 +5,225 @@ Combine several fits files into one master frame
 import datetime
 
 import astropy.io.fits as fits
+import matplotlib.pyplot as plt
 import numpy as np
+from scipy.optimize import curve_fit
 
+from clipnflip import clipnflip
 from modeinfo_uves import modeinfo_uves as modeinfo
+
+
+def gaussfit(x, y):
+    """ gaussian fit to data """
+    gauss = lambda x, A0, A1, A2: A0 * np.exp(-((x - A1) / A2)**2 / 2)
+    popt, _ = curve_fit(gauss, x, y, p0=[max(y), 1, 1])
+    return gauss(x, *popt), popt
+
+
+def gaussbroad(x, y, hwhm):
+    nw = len(x)
+    dw = (x[-1] - x[0]) / (len(x) - 1)
+
+    if hwhm > 5 * (x[-1] - x[0]):
+        return np.full(len(x), sum(y) / len(x))
+
+    nhalf = int(3.3972872 * hwhm / dw)
+    ng = 2 * nhalf + 1				        # points in gaussian (odd!)
+    # wavelength scale of gaussian
+    wg = dw * (np.arange(0, ng, 1, dtype=float) - (ng - 1) / 2)
+    xg = (0.83255461 / hwhm) * wg  # convenient absisca
+    gpro = (0.46974832 * dw / hwhm) * \
+        np.exp(-xg * xg)  # unit area gaussian w/ FWHM
+    gpro = gpro / np.sum(gpro)
+
+    # Pad spectrum ends to minimize impact of Fourier ringing.
+    npad = nhalf + 2				# pad pixels on each end
+    spad = np.concatenate((np.full(npad, y[0]), y, np.full(npad, y[-1]),))
+
+    # Convolve and trim.
+    sout = np.convolve(spad, gpro)  # convolve with gaussian
+    sout = sout[npad: npad + nw]  # trim to original data / length
+    return sout  # return broadened spectrum.
+
+
+def combine_flat(files, inst_setting, **kwargs):
+    """Combine several flat files into one master flat
+
+    Parameters
+    ----------
+    files : list(str)
+        flat files
+    inst_setting : str
+        instrument mode for modinfo
+    bias: array(int, float), optional
+        bias image to subtract from master flat (default: 0)
+    exten: {int, str}, optional
+        fits extension to use (default: 1)
+    xr: 2-tuple(int), optional
+        x range to use (default: None, i.e. whole image)
+    yr: 2-tuple(int), optional
+        y range to use (default: None, i.e. whole image)
+    Returns
+    -------
+    flat, fhead
+        image and header of master flat
+    """
+
+    flat, fhead = combine_frames(files, inst_setting, **kwargs)
+    flat = clipnflip(flat, fhead)
+    # Subtract master dark. We have to scale it by the number of Flats
+    bias = kwargs.get("bias", 0)
+    flat = flat - bias * len(files)  # subtract bias
+    flat = flat.astype(np.float32)  # Cast to smaller type to save disk space
+    return flat, fhead
+
+
+def combine_bias(files, inst_setting, **kwargs):
+    """
+    Combine bias frames, determine read noise, reject bad pixels.
+    Read noise calculation only valid if both lists yield similar noise.
+
+    Parameters
+    ----------
+    files : list(str)
+        bias files to combine
+    inst_setting : str
+        instrument mode for modinfo
+    exten: {int, str}, optional
+        fits extension to use (default: 1)
+    xr: 2-tuple(int), optional
+        x range to use (default: None, i.e. whole image)
+    yr: 2-tuple(int), optional
+        y range to use (default: None, i.e. whole image)
+    Returns
+    -------
+    bias, bhead
+        bias image and header
+    """
+
+    debug = kwargs.get("debug", False)
+
+    n = len(files) // 2
+    # necessary to maintain proper dimensionality, if there is just one element
+    if n == 0:
+        files = np.array([files, files])
+        n = 1
+    list1, list2 = files[:n], files[n:]
+
+    # Lists of images.
+    n1 = len(list1)
+    n2 = len(list2)
+    n = n1 + n2
+
+    # Separately images in two groups.
+    bias1, head1 = combine_frames(list1, inst_setting, **kwargs)
+    bias1 = clipnflip(bias1 / n1, head1)
+
+    bias2, head2 = combine_frames(list2, inst_setting, **kwargs)
+    bias2 = clipnflip(bias2 / n2, head2)
+
+    if bias1.ndim != bias2.ndim or bias1.shape[0] != bias2.shape[0]:
+        raise Exception(
+            'sumbias: bias frames in two lists have different dimensions')
+
+    # Make sure we know the gain.
+    head = head2
+    try:
+        gain = head['e_gain*']
+        gain = np.array([gain[i] for i in range(len(gain))])
+        gain = gain[0]
+    except KeyError:
+        gain = 1
+
+    # Construct unnormalized sum.
+    bias = bias1 * n1 + bias2 * n2
+
+    # Normalize the sum.
+    bias = bias / n
+
+    # Compute noise in difference image by fitting Gaussian to distribution.
+    diff = 0.5 * (bias1 - bias2)  # 0.5 like the mean...
+    if np.min(diff) != np.max(diff):
+
+        crude = np.median(np.abs(diff))  # estimate of noise
+        hmin = -5.0 * crude
+        hmax = +5.0 * crude
+        bin_size = np.clip(2 / n, 0.5, None)
+        nbins = int((hmax - hmin) / bin_size)
+
+        h, _ = np.histogram(diff, range=(hmin, hmax), bins=nbins)
+        xh = hmin + bin_size * (np.arange(0., nbins) + 0.5)
+
+        hfit, par = gaussfit(xh, h)
+        noise = abs(par[2])  # noise in diff, bias
+
+        # Determine where wings of distribution become significantly non-Gaussian.
+        contam = (h - hfit) / np.sqrt(np.clip(hfit, 1, None))
+        imid = np.where(abs(xh) < 2 * noise)
+        consig = np.std(contam[imid])
+
+        smcontam = gaussbroad(xh, contam, 0.1 * noise)
+        igood = np.where(smcontam < 3 * consig)
+        gmin = np.min(xh[igood])
+        gmax = np.max(xh[igood])
+
+        # Find and fix bad pixels.
+        ibad = np.where((diff <= gmin) | (diff >= gmax))
+        nbad = len(ibad[0])
+
+        bias[ibad] = np.clip(bias1[ibad], None, bias2[ibad])
+
+        # Compute read noise.
+        biasnoise = gain * noise
+        bgnoise = biasnoise * np.sqrt(n)
+
+        # Print diagnostics.
+        print('change in bias between image sets= %f electrons' %
+              (gain * par[1],))
+        print('measured background noise per image= %f' % bgnoise)
+        print('background noise in combined image= %f' % biasnoise)
+        print('fixing %i bad pixels' % nbad)
+
+        if debug:
+            # Plot noise distribution.
+            plt.subplot(211)
+            plt.plot(xh, h)
+            plt.plot(xh, hfit, c='r')
+            plt.title('noise distribution')
+            plt.axvline(gmin, c='b')
+            plt.axvline(gmax, c='b')
+
+            # Plot contamination estimation.
+            plt.subplot(212)
+            plt.plot(xh, contam)
+            plt.plot(xh, smcontam, c='r')
+            plt.axhline(3 * consig, c='b')
+            plt.axvline(gmin, c='b')
+            plt.axvline(gmax, c='b')
+            plt.title('contamination estimation')
+            plt.show()
+    else:
+        diff = 0
+        biasnoise = 1.
+        nbad = 0
+
+    obslist = files[0][0]
+    for i in range(1, len(files)):
+        obslist = obslist + ' ' + files[i][0]
+
+    try:
+        del head['tapelist']
+    except KeyError:
+        pass
+
+    head['bzero'] = 0.0
+    head['bscale'] = 1.0
+    head['obslist'] = obslist
+    head['nimages'] = (n, 'number of images summed')
+    head['npixfix'] = (nbad, 'pixels corrected for cosmic rays')
+    head['bgnoise'] = (biasnoise, 'noise in combined image, electrons')
+    bias = bias.astype(np.float32)
+    return bias, head
 
 
 def remove_bad_pixels(p, buff, row, nfil, ncol_a, rdnoise_amp, gain_amp, thresh):
@@ -57,7 +273,8 @@ def remove_bad_pixels(p, buff, row, nfil, ncol_a, rdnoise_amp, gain_amp, thresh)
 
     # Construct the summed flat.
     b = np.where(ibad, mfit, buff[:, :, row])
-    return np.sum(b, axis=0), nbad
+    b = np.sum(b, axis=0)
+    return b, nbad
 
 
 def calc_filwt(buffer):
@@ -106,7 +323,7 @@ def load_header(hdulist, exten=1):
     return head
 
 
-def combine_frames(files, instrument, exten=1, **kwargs):
+def combine_frames(files, instrument, **kwargs):
     """
     Subroutine to correct cosmic rays blemishes, while adding otherwise
     similar images.
@@ -133,6 +350,7 @@ def combine_frames(files, instrument, exten=1, **kwargs):
         show debug plot of noise distribution (default: False)
     """
 
+    exten = kwargs.get("exten", 1)
     debug = kwargs.get("debug")
     thres = kwargs.get("thres", 3.5)
     hwin = kwargs.get("hwin", 50)
@@ -180,12 +398,8 @@ def combine_frames(files, instrument, exten=1, **kwargs):
         head2, _ = modeinfo(head2, instrument, xr=xr, yr=yr)
         return bias2, head2
 
-# ===========================================================================
-# Initialize header information lists (one entry per file).
-    bzero = np.zeros(len(files))  # bzero list (0.0=default)
-    bscale = np.full(len(files), 1.)  # bscale list (1.0=default)
-    totalexpo = 0  # init total exposure time
-
+    # ===========================================================================
+    # Initialize header information lists (one entry per file).
     # Loop through files in list, grabbing and parsing FITS headers.
     # length of longest filename
     filename_length = np.max([len(l) for l in files])
@@ -195,32 +409,25 @@ def combine_frames(files, instrument, exten=1, **kwargs):
     fname = files[0]
     hdu = fits.open(fname)
     head2 = load_header(hdu)
-    head2, kw = modeinfo(head2, instrument, gain=True,
-                         orient=True, xr=xr, yr=yr)
-    gain, orient = kw["gain"], kw["orient"]
+    head2, kw = modeinfo(head2, instrument, xr=xr,
+                         yr=yr, gain=True, readn=True, orient=True)
+
+    # check if we deal with multiple amplifiers
+    n_ampl = head2.get('e_ampl', 1)
+
+    # section(s) of the detector to process
+    xlow = np.array(list(head2['e_xlo*'].values()), ndmin=1)
+    xhigh = np.array(list(head2['e_xhi*'].values()), ndmin=1)
+    ylow = np.array(list(head2['e_ylo*'].values()), ndmin=1)
+    yhigh = np.array(list(head2['e_yhi*'].values()), ndmin=1)
+
+    gain = np.array(list(head2["e_gain*"].values()), ndmin=1)
+    rdnoise = np.array(list(head2["e_readn*"].values()), ndmin=1)
+
+    nfix = 0  # init fixed pixel counter
 
     ncol_old = head2['naxis1']  # columns
     nrow_old = head2['naxis2']  # rows
-
-    # check if we deal with multiple amplifiers
-    try:
-        n_ampl = head2['e_ampl']
-    except KeyError:
-        n_ampl = 1
-    if n_ampl == 1:
-        # section(s) of the detector to process
-        xlow = [head2['e_xlo']]
-        xhigh = [head2['e_xhi']]
-        ylow = [head2['e_ylo']]
-        yhigh = [head2['e_yhi']]
-    else:
-        # section(s) of the detector to process
-        xlow = [i for i in head2['e_xlo*'].values()]
-        xhigh = [i for i in head2['e_xhi*'].values()]
-        ylow = [i for i in head2['e_ylo*'].values()]
-        yhigh = [i for i in head2['e_yhi*'].values()]
-
-    nfix = 0  # init fixed pixel counter
     bias2 = np.zeros((nrow_old, ncol_old))  # init r*4 output image array
 
     # check if non-linearity correction
@@ -231,45 +438,38 @@ def combine_frames(files, instrument, exten=1, **kwargs):
     # TODO: what happens for several amplifiers?
     # outer loop through amplifiers (note: 1,2 ...)
     for amplifier in range(n_ampl):
-        for ifile, fname in enumerate(files):  # loop though files
+        heads = [fits.open(f) for f in files]
+        heads = [load_header(h) for h in heads]
+        heads = [modeinfo(h, instrument, readn=True, gain=True, orient=True)[
+            0] for h in heads]
 
-            bias1 = fits.open(fname)
-            head2 = load_header(bias1)
-            head2, kw = modeinfo(head2, instrument, gain=True, orient=True)
-            gain, orient = kw["gain"], kw["orient"]
+        # Sanity Check
+        ncol = np.array([h['naxis1'] for h in heads])
+        nrow = np.array([h['naxis2'] for h in heads])
+        if np.any(ncol != ncol_old) or np.any(nrow != nrow_old):
+            raise Exception('Not all files have the same dimensions')
 
-            # Sanity check for the files
-            ncol = head2['naxis1']  # columns
-            nrow = head2['naxis2']  # rows
-            if nrow != nrow_old or ncol != ncol_old:
-                raise ValueError('sumfits: file ' + fname +
-                                 ' has different dimensions')
+        bscale = np.array([h.get("bscale", 1) for h in heads])
+        bzero = np.array([h.get("bzero", 0) for h in heads])
+        obj = [h["object"] for h in heads]
+        exposure = [h["exptime"] for h in heads]
+        totalexpo = sum(exposure)
 
-            # load some header data for each file
-            bscale[ifile] = head2.get("bscale", 1)  # pixel scale
-            bzero[ifile] = head2.get('bzero', 0)  # pixel offset
-            obj = head2['object']  # get object id
-            exposure = head2['EXPTIME']
-            totalexpo += exposure  # accumulate exposure times
-
-            print(fname, ifile, ncol, nrow, '  ', obj,
-                  exposure)  # summarize header info
+        # loop though files
+        for ifile, fname, nc, nr, ob, exp in zip(range(len(files)), files, ncol, nrow, obj, exposure):
+            print(fname, ifile, nc, nr, '  ', ob, exp)  # summarize header info
 
         xleft = xlow[amplifier]
         xright = xhigh[amplifier]
         ybottom = ylow[amplifier]
         ytop = yhigh[amplifier]
 
-        if n_ampl > 1:
-            gain_amp = gain[amplifier]
-            rdnoise_amp = rdnoise[amplifier]
-        else:
-            gain_amp = head2['e_gain']
-            rdnoise_amp = head2['e_readn']
+        gain_amp = gain[amplifier]
+        rdnoise_amp = rdnoise[amplifier]
 
-        orient = head2['e_orient']
+        orient = heads[0]['e_orient']
         if orient == 0 or orient == 2 or orient == 5 or orient == 7:
-            raise NotImplementedError #TODO implement, or better combine with other orientation
+            raise NotImplementedError  # TODO implement, or better combine with other orientation
             ncol_a = xright - xleft + 1
             nrow_a = ytop - ybottom + 1
 
@@ -375,7 +575,6 @@ def combine_frames(files, instrument, exten=1, **kwargs):
             c_row = np.arange(hwin, ytop - ybottom + 1) % (m_row)
             j_row = np.arange(2 * hwin, ytop - ybottom + 1) % (m_row)
 
-            # TODO Remove the loop, should be possible
             for i_row in range(ybottom + hwin, ytop - hwin + 1):
                 count = i_row - ybottom - hwin
                 if (i_row) % 100 == 0:
