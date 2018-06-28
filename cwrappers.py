a312f5bf68dc4d015c3c37f3184f771272da5165
Ansgar Wehrhahn <ansgar.wehrhahn@physics.uu.se>
2018-06-28 14:28:19 +0200

    Mask should really be fixed now

--- cwrappers.py
+++ cwrappers.py
@@ -14,30 +14,34 @@ import numpy as np
 from clib._cluster import ffi
 import clib._cluster.lib as clusterlib
 
+c_double = np.ctypeslib.ctypes.c_double
+c_int = np.ctypeslib.ctypes.c_int
+
 def find_clusters(img, min_cluster=4, filter_size=10, noise=1.0):
-    #img = img.T  # transpose input TODO: why?
+    # img = img.T  # transpose input TODO: why?
 
     if np.ma.is_masked(img):
-        mask = (~img.mask).astype(np.int32).flatten()
+        mask = (~img.mask).astype(c_int).flatten()
         mask = np.ascontiguousarray(mask)
-        mask = ffi.cast("int *", mask.ctypes.data)
+        cmask = ffi.cast("int *", mask.ctypes.data)
     else:
-        mask = ffi.cast("int *", np.ones_like(img).ctypes.data)
-
-    img = img.astype("i").flatten()
-    img = np.ascontiguousarray(img.data)
+        mask = np.ones(img.size, dtype=c_int)
+        cmask = ffi.cast("int *", mask.ctypes.data)
 
     nY, nX = img.shape
     nmax = np.inner(*img.shape) - np.ma.count_masked(img)
-    x = np.zeros(nmax, dtype="i")
-    y = np.zeros(nmax, dtype="i")
+    x = np.zeros(nmax, dtype=c_int)
+    y = np.zeros(nmax, dtype=c_int)
 
+    img = img.astype(c_int).flatten()
+    img = np.ascontiguousarray(img.data)
     cimg = ffi.cast("int *", img.ctypes.data)
     cx = ffi.cast("int *", x.ctypes.data)
     cy = ffi.cast("int *", y.ctypes.data)
 
-
-    n = clusterlib.locate_clusters(nX, nY, filter_size, cimg, nmax, cx, cy, noise, mask)
+    n = clusterlib.locate_clusters(
+        nX, nY, filter_size, cimg, nmax, cx, cy, noise, cmask
+    )
 
     x = x[:n]
     y = y[:n]
@@ -52,7 +56,8 @@ def find_clusters(img, min_cluster=4, filter_size=10, noise=1.0):
 
 if __name__ == "__main__":
     import matplotlib.pyplot as plt
-    img = np.zeros((101, 103), dtype='i') + 10
+
+    img = np.zeros((101, 103), dtype="i") + 10
 
     img[11:22, :] = 100
     img[80:90, 80:90] = 1
@@ -62,7 +67,7 @@ if __name__ == "__main__":
 
     cluster_img[x, y] = 255
 
-    #print(nclus, len(x), x, y, clusters)
+    # print(nclus, len(x), x, y, clusters)
 
     plt.subplot(121)
     plt.imshow(img)
