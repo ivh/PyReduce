c501c811afb5a27932f0cc520380a5b444851781
Ansgar Wehrhahn <ansgar.wehrhahn@physics.uu.se>
2018-07-10 15:39:22 +0200

    Some improvements to slitfunc.py

--- slitfunc.py
+++ slitfunc.py
@@ -1,10 +1,15 @@
 import numpy as np
+import logging
 from scipy.linalg import solve_banded
 
-def slitfunc(img, ycen, lambda_sp=0, lambda_sf=0.1, osample=1):
+
+def slitfunc(
+    img, ycen, lambda_sp=0, lambda_sf=0.1, osample=1, threshold=1e-5, max_iterations=20
+):
     """ Python implementation of the C slitfunction decomposition algorithm of Nikolai, mainly for testing """
-    im = img.data
-    mask = (~img.mask).astype(int)
+    im = np.ma.getdata(img)
+    mask = np.ma.getmask(img)
+    mask = (~mask).astype(int)
 
     nrows, ncols = img.shape
     ny = osample * (nrows + 1) + 1
@@ -13,11 +18,11 @@ def slitfunc(img, ycen, lambda_sp=0, lambda_sf=0.1, osample=1):
 
     sp = np.sum(img, axis=0).data / (img.size - np.ma.count_masked(img))
     sf = np.zeros(ny)
-    model = np.zeros((ncols, nrows))
+    model = np.zeros_like(im)
     unc = np.zeros(ncols)
 
     E = np.zeros(ncols)
-    sp_old = np.zeros(ncols)
+    # sp_old = np.zeros(ncols)
     Aij = np.zeros((nd, ny))
     Adiag = np.zeros((3, ncols))
     bj = np.zeros(ny)
@@ -26,47 +31,39 @@ def slitfunc(img, ycen, lambda_sp=0, lambda_sf=0.1, osample=1):
     # Populate omega
 
     iy = np.arange(ny)
+    iy2 = (1 - ycen) * osample
+    iy1 = iy2 - osample
+
+    d1 = np.where(iy2 == 0, step, np.where(iy1 == 0, 0, ycen % step))
+    d2 = step - d1
+
+    # TODO Which offset to pick here?
+    iy1 = iy1[:, None] + osample * np.arange(nrows)[None, :] #+ osample
+    iy2 = iy2[:, None] + osample * np.arange(nrows)[None, :] #+ osample
+
     for x in range(ncols):
-        iy2 = (1 - ycen[x]) * osample
-        iy1 = iy2 - osample
-        if iy2 == 0:
-            d1 = step
-        elif iy1 == 0:
-            d1 = 0.e0
-        else:
-            d1 = ycen[x] % step
-        d2 = step - d1
+        omega[iy[:, None] == iy1[None, x, :], x] = d1[x]
+        omega[(iy[:, None] > iy1[None, x, :]) & (iy[:, None] < iy2[None, x, :]), x] = step
+        omega[iy[:, None] == iy2[None, x, :], x] = d2[x]
 
-        #TODO ? offset
-        iy1 = iy1 + osample * np.arange(nrows) - osample
-        iy2 = iy2 + osample * np.arange(nrows) - osample
+    # Loop through sf, sp reconstruction until convergence is reached
+    for iteration in range(max_iterations):
+        ##  Compute slit function sf
 
-        omega[iy[:, None] == iy1[None, :], x] = d1
-        omega[(iy[:, None] > iy1[None, :]) & (iy[:, None] < iy2[None, :]), x] = step
-        omega[iy[:, None] == iy2[None, :], x] = d2
-
-    ## Loop through sf , sp reconstruction until convergence is reached */
-    iteration = 0
-    sp_change, sp_max = 1, 0
-    while iteration < 20 and sp_change > 1.e-5 * sp_max:
-        iteration += 1
-        ##  Compute slit function sf */
-
-        ## Fill in band-diagonal SLE array and the RHS */
-        #TODO more efficient way of doing this!
+        # Fill in band-diagonal SLE array and the RHS
         for iy in range(ny):
-            for jy in range(max(iy - osample, 0), min(iy + osample, ny - 1) + 1):
-                Aij[jy - iy + osample, iy] = np.sum(
-                    np.sum(omega[iy] * omega[jy] * mask, axis=0) * sp * sp
-                )
-
-            bj[iy] = np.sum(np.sum(omega[iy] * mask * im, axis=0) * sp)
+            mx = iy - osample if iy > osample else 0
+            mn = iy + osample + 1 if iy + osample < ny else ny
+            Aij[mx - iy + osample : mn - iy + osample, iy] = np.sum(
+                omega[iy] * omega[mx:mn] * mask * sp * sp, axis=(1, 2)
+            )
+            bj[iy] = np.sum(omega[iy] * mask * im * sp)
 
         diag_tot = np.sum(Aij[osample, :])
 
         ## Scale regularization parameters */
 
-        lamb = lambda_sf * diag_tot / ny
+        lamb = lambda_sf * diag_tot / ny * osample
 
         ## Add regularization parts for the SLE matrix */
         # Franklin regularization? (A + lambda * D) x = b
@@ -75,72 +72,68 @@ def slitfunc(img, ycen, lambda_sp=0, lambda_sf=0.1, osample=1):
         Aij[osample, 0] += lamb
         Aij[osample, 1:-1] += lamb * 2
         Aij[osample, -1] += lamb
-
         # Upper diagonal */
-        Aij[osample + 1, 0] -= lamb
-        Aij[osample + 1, 1:-1] -= lamb
-
+        Aij[osample + 1, :-1] -= lamb
         # Lower diagonal */
-        Aij[osample - 1, 1:-1] -= lamb
-        Aij[osample - 1, -1] -= lamb
+        Aij[osample - 1, 1:] -= lamb
 
         # Solve the system of equations */
-        tmp = (nd - 1) // 2
-        bj = solve_banded((tmp, tmp), Aij, bj)
+        bj = solve_banded((osample, osample), Aij, bj)
 
         # Normalize the slit function */
-        sf = np.copy(bj) / np.sum(bj) * osample
+        sf = bj / np.sum(bj) * osample
 
         #  Compute spectrum sp */
-        sum = np.sum(omega * sf[:, None, None], axis=0)
-        Adiag[1, :] = np.sum(sum * sum * mask, axis=0)
-        E = np.sum(sum * img, axis=0)
+        tmp = np.sum(omega * sf[:, None, None], axis=0)
+        Adiag[1, :] = np.sum(tmp * tmp * mask, axis=0)
+        E = np.sum(tmp * im * mask, axis=0)
 
-        if lambda_sp > 0.e0:
-            norm = 0.e0
+        if lambda_sp > 0.:
             sp_old = np.copy(sp)
             norm = np.sum(sp) / ncols
 
             lamb = lambda_sp * norm
-            Adiag[0, 0] = 0.e0
-            Adiag[1, 0] += lamb
-            Adiag[2, 0] -= lamb
-            Adiag[0, 1:-1] -= lamb
-            Adiag[1, 1:-1] += 2.e0 * lamb
-            Adiag[2, 1:-1] -= lamb
-            Adiag[0, -1] -= lamb
-            Adiag[2, -1] += lamb
-            Adiag[3, -1] = 0.e0
+            Adiag[0, 1:] -= lamb
 
-            E = solve_banded((1, 1), Adiag, E)
-            sp = np.copy(E)
+            Adiag[1, 0] += lamb
+            Adiag[1, 1:-1] += 2 * lamb
+            Adiag[1, -1] += lamb
+
+            Adiag[2, :-1] -= lamb
+
+            sp = solve_banded((1, 1), Adiag, E)
+            # sp = np.copy(E)
         else:
             sp_old = np.copy(sp)
             sp = E / Adiag[1]
 
         # Compute the model */
-        model = np.sum(omega * sf[:, None, None], axis=0) * sp[None, :]
+        #tmp = np.sum(omega * sf[:, None, None], axis=0)
+        model[:] = tmp * sp[None, :]
 
         # Compare model and data */
-
-        #sum = np.sum(mask * (model - im) * (model - im))
-        #isum = np.sum(mask)
-        #dev = np.sqrt(sum / isum)
-        dev = np.std(model - img)
+        tmp = (model - im) * mask
+        dev = np.std(tmp)
 
         # Adjust the mask marking outlyers */
-
-        mask[np.abs(model - img) > 6 * dev] = 0
-        mask[np.abs(model - img) <= 6 * dev] = 1
+        idx_bad = np.abs(model - im) > 6 * dev
+        mask[idx_bad] = 0
+        mask[~idx_bad] = 1
 
         # Compute the change in the spectrum */
         sp_max = np.max(sp)
         sp_change = np.max(np.abs(sp - sp_old))
-        ## Check the convergence */
 
-    unc = np.sum((model - img) * (model - img), axis=0)
+        #logging.debug("Iteration: %i", iteration)
+        
+        ## Check the convergence */
+        if sp_change < threshold * sp_max:
+            break
+
+    logging.debug("iterations = %i, sp_max = %f, sp_change = %f", iteration, sp_max, sp_change)
+    tmp = (model - im) * mask
+    unc = np.sum(tmp * tmp, axis=0)
     unc = np.sqrt(unc * nrows)
 
-    model.shape = img.shape
     img.mask = ~mask.astype(bool)
     return sp, sf, model, unc
