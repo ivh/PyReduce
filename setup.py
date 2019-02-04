07f1cf492c72b55abf22795b1b67cc06c4aaf9b5
Ansgar Wehrhahn <ansgar.wehrhahn@physics.uu.se>
2019-02-04 17:36:35 +0100

    Update travis CI

--- setup.py
+++ setup.py
@@ -4,9 +4,12 @@ Compiles the C functions
 """
 from setuptools import setup
 
-from pyreduce.clib import build_cluster, build_extract
 
-build_cluster.build()
+# from pyreduce.clib import build_cluster
+# build_cluster.build()
+
+from pyreduce.clib import build_extract
+
 build_extract.build()
 
 setup(name="pyreduce", version="0.0", packages=["pyreduce"])
