ea5e03c5a48ea0149b6654908bd04a924ad889bb
Ansgar Wehrhahn <ansgar.wehrhahn@physics.uu.se>
2019-02-04 17:49:25 +0100

    Missed a file

--- setup.py
+++ setup.py
@@ -8,4 +8,8 @@ from setuptools import setup
 # from pyreduce.clib import build_cluster
 # build_cluster.build()
 
+# from .pyreduce.clib import build_extract
+
+# build_extract.build()
+
 setup(name="pyreduce", version="0.0", packages=["pyreduce"])
