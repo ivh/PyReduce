35db0d5ef357f76ff2d932f22973d05ad3edfe4d
Ansgar Wehrhahn <ansgar.wehrhahn@physics.uu.se>
2019-06-18 15:48:16 +0200

    First CRIRES+ support draft (not finished)

---
+++ examples/crires_plus_example.py
@@ -0,0 +1,52 @@
+"""
+Simple usage example for PyReduce
+Loads a sample UVES dataset, and runs the full extraction
+"""
+
+import os.path
+import pyreduce
+from pyreduce import datasets
+
+
+# define parameters
+instrument = "Crires+"
+target = "Vega"
+night = "all"
+mode = "K"
+steps = (
+    "bias",
+    "flat",
+    "orders",
+    "norm_flat",
+    "wavecal",
+    # "freq_comb",
+    "curvature",
+    "science",
+    "continuum",
+    "finalize",
+)
+
+# some basic settings
+# Expected Folder Structure: base_dir/datasets/HD132205/*.fits.gz
+# Feel free to change this to your own preference, values in curly brackets will be replaced with the actual values {}
+
+# load dataset (and save the location)
+base_dir = "/DATA/ESO_Archive/CRIRES+"
+input_dir = "demodata/"
+output_dir = "reduced/{mode}/"
+
+# Path to the configuration parameters, that are to be used for this reduction
+config = os.path.join(os.path.dirname(__file__), "settings_CRIRES_PLUS.json")
+
+pyreduce.reduce.main(
+    instrument,
+    target,
+    night,
+    mode,
+    steps,
+    base_dir=base_dir,
+    input_dir=input_dir,
+    output_dir=output_dir,
+    configuration=config,
+    # order_range=(0, 25),
+)
---
+++ pyreduce/instruments/crires_plus.py
@@ -0,0 +1,170 @@
+"""
+Handles instrument specific info for the HARPS spectrograph
+
+Mostly reading data from the header
+"""
+import os.path
+import glob
+import logging
+from datetime import datetime
+import fnmatch
+import json
+
+import numpy as np
+from astropy.io import fits
+from dateutil import parser
+
+from .common import getter, instrument, observation_date_to_night
+
+
+class CRIRES_PLUS(instrument):
+    def __init__(self):
+        self.instrument = "crires_plus"
+
+    def add_header_info(self, header, mode, **kwargs):
+        """ read data from header and add it as REDUCE keyword back to the header """
+        # "Normal" stuff is handled by the general version, specific changes to values happen here
+        # alternatively you can implement all of it here, whatever works
+        header = super().add_header_info(header, mode)
+        info = self.load_info()
+
+        return header
+
+    def sort_files(self, input_dir, target, night, mode):
+        """
+        Sort a set of fits files into different categories
+        types are: bias, flat, wavecal, orderdef, spec
+
+        Parameters
+        ----------
+        input_dir : str
+            input directory containing the files to sort
+        target : str
+            name of the target as in the fits headers
+        night : str
+            observation night, possibly with wildcards
+        mode : str
+            instrument mode
+        Returns
+        -------
+        files_per_night : list[dict{str:dict{str:list[str]}}]
+            a list of file sets, one entry per night, where each night consists of a dictionary with one entry per setting,
+            each fileset has five lists of filenames: "bias", "flat", "order", "wave", "spec", organised in another dict
+        nights_out : list[datetime]
+            a list of observation times, same order as files_per_night
+        """
+
+        info = self.load_info()
+        target = target.upper()
+        instrument = info["__instrument__"].upper()
+
+        id_orddef = info["id_orddef"]
+        id_flat = info["id_flat"]
+        id_spec = info["id_spec"]
+
+        # Try matching with nights
+        try:
+            night = parser.parse(night).date()
+            individual_nights = [night]
+        except ValueError:
+            # if the input night can't be parsed, use all nights
+            # Usually the case if wildcards are involved
+            individual_nights = "all"
+
+        # find all fits files in the input dir(s)
+        input_dir = input_dir.format(
+            instrument=instrument.upper(), target=target, mode=mode, night=night
+        )
+        files = glob.glob(input_dir + "/*.fits")
+        files += glob.glob(input_dir + "/*.fits.gz")
+        files = np.array(files)
+
+        # Load the mode identifier for the current mode from the header
+        # This could be anything really, e.g. the size of the data axis
+        i = [i for i, m in enumerate(info["modes"]) if m == mode][0]
+        mode_id = info["modes_id"][i].upper()
+
+        # Initialize arrays
+        # observed object
+        ob = np.zeros(len(files), dtype="U20")
+        # observation type, bias, flat, spec, etc.
+        ty = np.zeros(len(files), dtype="U20")
+        # instrument mode, e.g. red, blue
+        mo = np.zeros(len(files), dtype="U20")
+        # special setting identifier, e.g. wavelength setting
+        se = np.zeros(len(files), dtype="U20")
+        # observed night, parsed into a datetime object
+        ni = np.zeros(len(files), dtype=datetime)
+        # instrument, used for observation
+        it = np.zeros(len(files), dtype="U20")
+
+        for i, f in enumerate(files):
+            h = fits.open(f)[0].header
+            ob[i] = h.get(info["target"], "")
+            ty[i] = h.get(info["observation_type"], "")
+            # The mode descriptor has different names in different files, so try different ids
+            mo[i] = h.get(info["instrument_mode"])
+            if mo[i] is None:
+                mo[i] = h.get(info["instrument_mode_alternative"], "")[:3]
+            ni_tmp = h.get(info["date"], "")
+            it[i] = h.get(info["instrument"], "")
+            se[i] = "HARPS"
+
+            # Sanitize input
+            ni[i] = observation_date_to_night(ni_tmp)
+            ob[i] = ob[i].replace("-", "")
+
+        if isinstance(individual_nights, str) and individual_nights == "all":
+            individual_nights = np.unique(ni)
+            logging.info(
+                "Can't parse night %s, use all %i individual nights instead",
+                night,
+                len(individual_nights),
+            )
+
+        files_per_night = []
+        nights_out = []
+        for ind_night in individual_nights:
+            # Select files for this night, this instrument, this instrument mode
+            selection = (ni == ind_night) & (it == instrument) & (mo == mode_id)
+
+            # Find all unique setting keys for this night and target
+            # Only look at the settings of observation files
+            match_ty = np.array([fnmatch.fnmatch(t, id_spec) for t in ty])
+            match_ob = np.array([fnmatch.fnmatch(t, target) for t in ob])
+
+            keys = se[match_ty & match_ob & selection]
+            keys = np.unique(keys)
+
+            files_this_night = {}
+            for key in keys:
+                select = selection & (se == key)
+
+                # find all relevant files for this setting
+                # bias ignores the setting
+                files_this_night[key] = {
+                    "bias": files[(ty == info["id_bias"]) & selection],
+                    "flat": files[(ty == id_flat) & select],
+                    "orders": files[(ty == id_orddef) & select],
+                    "wavecal": files[(ty == info["id_wave"]) & select],
+                    "freq_comb": files[(ty == info["id_comb"]) & select],
+                    "science": files[match_ty & match_ob & select],
+                }
+                files_this_night[key]["curvature"] = (
+                    files_this_night[key]["freq_comb"]
+                    if len(files_this_night[key]["freq_comb"]) != 0
+                    else files_this_night[key]["wavecal"]
+                )
+
+            if len(keys) != 0:
+                nights_out.append(ind_night)
+                files_per_night.append(files_this_night)
+
+        return files_per_night, nights_out
+
+    def get_wavecal_filename(self, header, mode, **kwargs):
+        """ Get the filename of the wavelength calibration config file """
+        cwd = os.path.dirname(__file__)
+        fname = "{instrument}_{mode}_2D.npz".format(instrument="harps", mode=mode)
+        fname = os.path.join(cwd, "..", "wavecal", fname)
+        return fname
