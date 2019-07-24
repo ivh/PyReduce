d41c4a7fd98ac7e03693bc8b2e3ffaaed14a7326
Ansgar Wehrhahn <ansgar.wehrhahn@physics.uu.se>
2019-07-24 16:20:19 +0200

    Tools changes/improvements for NIRSPEC

--- tools/bpm_creator.py
+++ tools/bpm_creator.py
@@ -3,14 +3,46 @@ import numpy as np
 from astropy.io import fits
 import matplotlib.pyplot as plt
 
+from scipy.ndimage import morphology
+
 class SelectorWindow:
     def __init__(self, data):
         self.data = data
         self.shape = data.shape
         self.mask = np.full(self.shape, False)
+        self.first_guess()
         self.fig, self.ax = plt.subplots()
         self.plot()
         self.connect()
+
+    def first_guess(self):
+        self.mask[self.data > np.percentile(self.data, 99.)] = True
+        self.mask[self.data < np.percentile(self.data, 1.)] = True
+
+        self.mask[1::4, :512] = True
+        
+
+        # struct = np.full((1, 3), 1)
+        # self.mask = morphology.binary_opening(self.mask, struct, border_value=0)
+
+        struct = np.full((1, 3), 1)
+        self.mask = morphology.binary_closing(self.mask, struct, border_value=1)
+
+        # struct = np.full((1, 5), 1)
+        # self.mask = morphology.binary_opening(self.mask, struct, border_value=0)
+
+        # struct = np.full((1, 100), 1)
+        # self.mask = morphology.binary_closing(self.mask, struct, border_value=1)
+
+        # # struct = np.full((1, 3), 1)
+        # # self.mask = morphology.binary_opening(self.mask, struct, border_value=0)
+
+        # # struct = np.full((1, 300), 1)
+        # # self.mask = morphology.binary_closing(self.mask, struct, border_value=1)
+
+
+
+
 
     def plot(self):
         if len(self.ax.images) != 0:
@@ -21,7 +53,7 @@ class SelectorWindow:
             xlim = 0, self.data.shape[1]
         self.ax.clear()
         tmp = np.ma.masked_array(self.data, mask=self.mask)
-        self.ax.imshow(tmp, origin="lower")
+        self.ax.imshow(tmp, origin="lower", vmin=self.data.min(), vmax=self.data.max())
         self.ax.set_xlim(xlim)
         self.ax.set_ylim(ylim)
         self.fig.canvas.draw()
@@ -44,24 +76,33 @@ class SelectorWindow:
         self.mask[y, x] = ~self.mask[y, x]
         self.plot()
 
+def load(fname, extension=0):
+    data = fits.getdata(fname, ext=extension)
+    return data
+
 if __name__ == "__main__":
     # Load data
     # TODO have file selector in toolbar
-    instrument = "JWST_NIRISS"
-    mode = "GR700XD"
-    base_dir = os.path.expanduser("~/Documents/Visual Studio Code/PyReduce/datasets")
-    fname = os.path.join(base_dir, instrument, "raw", "niriss_gj436_0.fits.gz")
-    fname_out = f"mask_{instrument.lower()}_{mode.lower()}.fits"
+    instrument = "NIRSPEC"
+    mode = "NIRSPEC"
+    # base_dir = os.path.expanduser(f"/DATA/Keck/{instrument}/GJ1214_b/")
+    # fname = os.path.join(base_dir, "raw", "cal", "NS.20100805.07223.fits.gz")
+    
+    fname = ["/DATA/Keck/NIRSPEC/GJ1214_b/raw/cal/NS.20100816.08690.fits.gz",
+             "/DATA/Keck/NIRSPEC/GJ1214_b/raw/cal/NS.20100816.08704.fits.gz",
+             "/DATA/Keck/NIRSPEC/GJ1214_b/raw/cal/NS.20100816.08718.fits.gz"]
+    fname_out = f"mask_{instrument.lower()}_{mode.lower()}.fits.gz"
     extension = 0
 
-    hdu = fits.open(fname)
-    header = hdu[0].header
-    data = hdu[extension].data
+    data = [load(f) for f in fname]
+    data = np.sum(data, axis=0)
+
+    header = fits.getheader(fname[0])
 
     # Create mask
     sw = SelectorWindow(data)
     plt.show()
-    mask = np.where(sw.mask, 0, 1, dtype="uint8")
+    mask = np.where(sw.mask, 0, 1).astype(dtype="uint8")
 
     # Save mask
-    fits.writeto(fname_out, header=header, data=mask, overwrite=True)
+    fits.writeto(fname_out, header=header, data=mask, overwrite=True, output_verify="fix+warn")
--- tools/wavecal_creator.py
+++ tools/wavecal_creator.py
@@ -2,23 +2,49 @@ from astropy.io import fits
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy import signal
+import time
 
 
 # in: wavelength observation, linelist
 # out: linelist
 class OverallPlot:
-    def __init__(self, data, linelist):
+    def __init__(self, data, linelist, ax=None, lines=None):
         self.data = data
         self.linelist = linelist
         self.nord, self.ncol = data.shape
 
-        self.fig, self.ax = plt.subplots()
+        if ax is None:
+            self.fig, self.ax = plt.subplots()
+        else:
+            self.ax = ax
+            self.fig = ax.figure
+
+        if lines is None:
+            self.lines = np.array([[0, 6, 23760.12, 1, 3, -5, 5], [self.ncol, 6, 24106.88, 1, 3, self.ncol-5, self.ncol + 5], #32
+                                   [0, 5, 23045.63, 1, 3, -5, 5], [self.ncol, 5, 23380.86, 1, 3, self.ncol-5, self.ncol + 5], #33
+                                   [0, 4, 22373.16, 1, 3, -5, 5], [self.ncol, 4, 22697.54, 1, 3, self.ncol-5, self.ncol + 5], #34
+                                   [0, 3, 21739.12, 1, 3, -5, 5], [self.ncol, 3, 22053.27, 1, 3, self.ncol-5, self.ncol + 5], #35
+                                   [0, 2, 21140.30, 1, 3, -5, 5], [self.ncol, 2, 21444.79, 1, 3, self.ncol-5, self.ncol + 5], #36
+                                   [0, 1, 20573.86, 1, 3, -5, 5], [self.ncol, 1, 20869.20, 1, 3, self.ncol-5, self.ncol + 5], #37
+                                   [0, 0, 20037.22, 1, 3, -5, 5], [self.ncol, 0, 20323.91, 1, 3, self.ncol-5, self.ncol + 5], #38
+                                ])
+
+            # self.lines[:, :, 1] -= (self.lines[..., 1] - self.lines[..., 0]) / 3
+        else:
+            self.lines = lines
+
+        self.ax.__lines__ = self.lines
+
         self.plot()
         self.connect()
+        self.op = None
 
     def plot(self):
+        self.ax.clear()
         self.ax.imshow(self.data, origin="lower", aspect="auto")
         self.fig.canvas.draw()
+        self.fig.canvas.flush_events()
+
 
     def connect(self):
         """ connect the click event with the appropiate function """
@@ -26,61 +52,77 @@ class OverallPlot:
             "button_press_event", self.on_click
         )
 
+    def disconnect(self):
+        self.fig.canvas.mpl_disconnect(self.cidclick)
+
     def on_click(self, event):
         if event.ydata is None:
             return
         y = int(np.round(event.ydata))
 
-        # Ask for wavelength region (if not defined)
-        wave0 = 25273 #input("Start wavelength")
-        wave1 = 25961 #input("End wavelength")
 
         # Open new window with just the selected order
-        op = OrderPlot(data[y], self.linelist, (wave0, wave1), y)
-        plt.show()
+        self.disconnect()
+        self.op = OrderPlot(data, y, self.linelist, self.lines, ax=self.ax)
 
 
 class OrderPlot:
-    def __init__(self, data, linelist, wave_region, order):
-        self.data = data / np.max(data)
-        self.ncol = len(data)
+    def __init__(self, data, order, linelist, lines, ax=None):
+        self.overall_data = data
+        self.data = data[order] / np.max(data[order])
+        self.ncol = len(self.data)
         self.order = order
 
-        height = 2 * np.ma.median(data)
-        self.peaks, _ = signal.find_peaks(data, height=height, width=3)
+        height = 2 * np.ma.median(self.data)
+        self.peaks, _ = signal.find_peaks(self.data, height=height, width=3)
 
         self.linelist = linelist
-        self.lines = np.array([[0, wave_region[0]], [self.ncol, wave_region[1]]])
+        self.lines = lines
         self.cutlines = False
 
-        self.poly = np.polyfit(self.pos, self.wave, 1)
+        self.poly = np.polyfit(self.pos, self.wave, self.degree)
 
-        self.fig, self.ax = plt.subplots()
+        if ax is None:
+            self.fig, self.ax = plt.subplots()
+        else:
+            self.fig = ax.figure
+            self.ax = ax
+
         self.connect()
 
         self.plot()
+        self.op = None
+        self.firstclick = True
 
 
     @property
     def nlines(self):
-        return len(self.lines)
+        return len(self.lines[self.lines[:, 1] == self.order])
 
     @property
     def pos(self):
-        return self.lines[:, 0]
+        return self.lines[self.lines[:, 1] == self.order, 0]
 
     @property
     def wave(self):
-        return self.lines[:, 1]
+        return self.lines[self.lines[:, 1] == self.order, 2]
+
+    @property
+    def degree(self):
+        return min(5, max(self.nlines - 1, 0)) 
 
     def plot(self):
+        self.ax.clear()
+
         wave = np.polyval(self.poly, np.arange(self.ncol))
         reference = np.zeros(self.ncol)
 
-        for line in self.linelist:
+        for i, line in enumerate(self.linelist):
             wl = line["wll"]
             if wl > wave[0] and wl < wave[-1]:
-                middle = np.digitize(wl, wave)
+                middle = np.argmin(np.abs(wl - wave))
+                self.linelist["posc"][i] = middle
+                self.linelist["order"][i] = self.order
                 first = int(np.clip(middle - line["width"], 0, self.ncol))
                 last = int(np.clip(middle + line["width"], 0, self.ncol))
                 reference[first:last] += (
@@ -89,10 +131,13 @@ class OrderPlot:
         reference /= np.max(reference)
 
         plt.title(f"Order {self.order}")
+        plt.xlim(0, len(self.data))
         plt.plot(self.data, label="data")
         plt.plot(self.peaks, self.data[self.peaks], "d", label="peaks")
         plt.plot(reference, label="reference")
         plt.legend()
+
+        self.fig.canvas.draw()
 
     def connect(self):
         """ connect the click event with the appropiate function """
@@ -100,32 +145,47 @@ class OrderPlot:
             "button_press_event", self.on_click
         )
 
+    def disconnect(self):
+        self.fig.canvas.mpl_disconnect(self.cidclick)
+
     def on_click(self, event):
-        print("BLA")
         if event.xdata is None:
             return
-        x = int(np.round(event.xdata))
+        # Right Click
+        if event.button == 3:
+            self.disconnect()
+            self.op = OverallPlot(self.overall_data, self.linelist, ax=self.ax, lines=self.lines)
+        # Left click
+        if event.button == 1:
+            if self.firstclick:
+                x = int(np.round(event.xdata))
+                # find closest peak
+                idx = np.argmin(np.abs(x - self.peaks))
+                line, = plt.plot([self.peaks[idx],], [self.data[self.peaks[idx]],], "rx")
+                self.ax.figure.canvas.draw()
+                self.fig.canvas.flush_events()
+                self.firstclick = False
 
-        # find closest peak
-        idx = np.argmin(np.abs(x - self.peaks))
+                self.position = self.peaks[idx]
+                self.height = self.data[self.peaks[idx]]
+            else:
+                x = int(np.round(event.xdata))
+                order_lines = self.linelist[self.linelist["order"] == self.order]
+                idx = np.argmin(np.abs(x - order_lines["posc"]))
+                # find closest peak
+                line, = plt.plot(order_lines["posc"][idx], 1, "rx")
+                self.ax.figure.canvas.draw()
+                self.fig.canvas.flush_events()
+                wave = order_lines["wll"][idx]
+                print(wave)
 
-        # find closest line in linelist
-        wave = np.polyval(self.poly, np.arange(self.ncol))
-        pos = np.digitize(self.linelist["wll"], wave)
-        idx2 = np.argmin(np.abs(pos - self.peaks[idx]))
-        wl = self.linelist[idx2]["wll"]
+                # connect this peak in the observation with the (nearest) line in the linelist
+                self.lines = np.concatenate((self.lines, [[self.position, self.order, wave, self.height, 3, self.position - 5, self.position + 5]]))
 
-        # connect this peak in the observation with the (nearest) line in the linelist
-        self.lines = np.concatenate((self.lines, [[self.peaks[idx], wl]]))
+                self.poly = np.polyfit(self.pos, self.wave, deg=self.degree)
+                self.plot()
 
-        # Remove the initial wavelength guess
-        if not self.cutlines and self.nlines >= 4:
-            self.lines = self.lines[2:]
-            self.cutlines = True
-
-        degree = min(5, self.nlines - 1)
-        self.poly = np.polyfit(self.pos, self.wave, deg=degree)
-        self.plot()
+                self.firstclick = True
 
 def air2vac(wl_air):
     """
@@ -178,6 +238,15 @@ if __name__ == "__main__":
 
     op = OverallPlot(data, linelist)
     plt.show()
+
+    lines = op.ax.__lines__
+    # Remove initial guesses
+    lines = lines[2*7:]
+    posc, order, wll, height, width, xfirst, xlast = lines.T
+    flag = np.full(len(posc), True)
+    posm = np.copy(posc)
+    lines = np.rec.fromarrays((posc, order, wll, height, width, xfirst, xlast, posm, flag), names=["posc", "order", "wll", "height", "width", "xfirst", "xlast", "posm", "flag"])
+    np.savez("lines.npz", cs_lines=lines)
 
     # Plot wl spectrum, without linelist
     # Go to each order, and ask for a wavelength range
