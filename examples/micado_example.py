c09b73af56bb37c9cc3663e058c92b696591c316
nadsabha <66068100+nadsabha@users.noreply.github.com>
2023-02-17 16:42:01 +0100

    Add MICADO example

---
+++ examples/micado_example.py
@@ -0,0 +1,69 @@
+"""
+Simple usage example for PyReduce
+Loads a ScopeSim simulated MICADO dataset (with updated spectral layout and updated line lists), and runs the full extraction. 
+"""
+
+import os.path
+import pyreduce
+from pyreduce import datasets
+
+
+# define parameters
+instrument = "MICADO"
+target = ""
+night = ""
+mode = ""
+steps = (
+     # "bias",
+     "flat",
+     "orders",
+     "curvature",
+     # # "scatter",
+     # "norm_flat", 
+     "wavecal",
+     # "science",
+     # "continuum",
+     # "finalize",
+)
+
+# Some basic settings
+# Expected Folder Structure: base_dir/datasets/MICADO/*.fits.gz
+# Feel free to change this to your own preference, values in curly brackets will be replaced with the actual values {}
+
+# Define the path for the base, input and output directories
+# The data (with fixed header keywords) can be fetched from https://www.dropbox.com/sh/e3lnvtkmyjveajk/AABPHxeUdDO5AnkWCAjbM0e1a?dl=0 and stored in input_dir
+
+#PC
+base_dir ="/media/data/Dropbox/Dropbox/WORKING/iMICADO/Working/WORKING_PyReduce/DATA/datasets/MICADO/"# an example path which you should change to your prefered one 
+
+input_dir = "raw_new/HK/"
+output_dir = "reduced_new/"
+
+config = pyreduce.configuration.get_configuration_for_instrument(instrument, plot=1)
+
+
+# Configuring parameters of individual steps here overwrites those defined  in the settings_MICADO.json file. 
+# Once you are satisfied with a certain parameter, you can update it in settings_MICADO.json.
+
+
+# config["orders"]["noise"] = 100
+# config["curvature"]["extraction_width"] = 350 # curvature can still be improved with this and the following parameters
+# config["curvature"]["peak_threshold"] =10  
+# config["curvature"]["peak_width"] =2 #CHECK 6 also works and detects one less line
+# config["curvature"]["window_width"] = 5
+# config["wavecal"]["extraction_width"] = 350
+
+#NOTE: micado.thar_master.fits (created and controlled by wavecal_master) is not overwritten if any parameter in the steps before it are changed.
+
+pyreduce.reduce.main(
+    instrument,
+    target,
+    night,
+    mode,
+    steps,
+    base_dir=base_dir,
+    input_dir=input_dir,
+    output_dir=output_dir,
+    configuration=config,
+    order_range=(3, 4),     #for MICADO, when one order is on the detector
+)
