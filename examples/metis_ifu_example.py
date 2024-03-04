c0522a3a667cba98cbdf0d2cfee7a67549617938
Thomas Marquart <thomas@marquart.se>
2024-03-04 11:52:58 +0100

    more metis split lss from ifu

--- examples/metis_ifu_example.py
+++ examples/metis_ifu_example.py
@@ -34,7 +34,7 @@ steps = (
 # Feel free to change this to your own preference, values in curly brackets will be replaced with the actual values {}
 
 # Define the path for the base, input and output directories
-# The data can be fetched from https://www.dropbox.com/sh/h1dz80vsw4lwoel/AAAqJD_FGDGC-t12wgnPXVR8a?dl=0 and stored in /raw/
+# The da://neon.physics.uu.se/metis/lms_pinholes.fitsta can be fetched from https://www.dropbox.com/sh/h1dz80vsw4lwoel/AAAqJD_FGDGC-t12wgnPXVR8a?dl=0 and stored in /raw/
 
 #laptop
 base_dir = "/Users/Nadeen/Dropbox/WORKING/iMETIS/Working/WORKING_PyReduce/DATA/datasets/METIS/" # an example path which you should change to your prefereed one
