7957902383a6194c9d54fce618b7f2eb663cdc72
Thomas Marquart <thomas@marquart.se>
2026-01-03 10:54:32 +0100

    Fix test_normflat to use column_range returned by extract

--- test/test_normflat.py
+++ test/test_normflat.py
@@ -15,7 +15,7 @@ def test_normflat(flat, orders, settings, order_range, scatter, instrument):
     if flat[0] is None:
         pytest.skip(f"No flat exists for instrument {instrument}")
 
-    norm, _, blaze, _ = extract(
+    norm, _, blaze, extracted_column_range = extract(
         flat,
         orders,
         scatter=scatter,
@@ -43,12 +43,11 @@ def test_normflat(flat, orders, settings, order_range, scatter, instrument):
 
     assert isinstance(blaze, np.ndarray)
     assert blaze.ndim == 2
-    assert blaze.shape[0] == order_range[1] - order_range[0]
+    assert blaze.shape[0] == len(extracted_column_range)
     assert blaze.shape[1] == flat.shape[1]
     assert np.issubdtype(blaze.dtype, np.floating)
     assert not np.any(np.isnan(blaze))
 
-    for i, j in enumerate(range(order_range[0], order_range[1])):
-        cr = column_range[j]
+    for i, cr in enumerate(extracted_column_range):
         assert np.all(blaze[i, : cr[0]].mask)
         assert np.all(blaze[i, cr[1] :].mask)
